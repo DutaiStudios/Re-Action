a2db918b6059b79275dc15879e4d96bdfb071670
JSchoppe <jethro.schoppenhorst@gmail.com>
2019-01-04 21:58:58 -0600

    Basic Implementation of Buoyancy
    liquid volume trigger, without current, implemented

---
+++ Prototype/Assets/Code/ForPrefabs/LiquidVolume/LiqVolume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiqVolume : MonoBehaviour
+{
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.GetComponent<Rigidbody>().AddForce(Vector3.up * Time.fixedDeltaTime * 15, ForceMode.Impulse);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Prototype/Assets/Code/Jethro Testing/EnergyTool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyTool : MonoBehaviour
+{
+    // Bools to track which energies are currently available.
+    private bool kineticEnergyUnlocked;
+    private bool thermalEnergyUnlocked;
+    private bool magneticEnergyUnlocked;
+
+
+
+    bool timeSlow = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.T))
+        {
+            timeSlow = !timeSlow;
+
+            if (timeSlow)
+            {
+                Time.timeScale = 0.1f;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+	}
+}
