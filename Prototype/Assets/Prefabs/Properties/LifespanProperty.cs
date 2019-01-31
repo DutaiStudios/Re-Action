960429e0d01e272f2060a51f1a103ed8b0caf00b
JSchoppe <jethro.schoppenhorst@gmail.com>
2019-01-31 01:22:10 -0600

    Restructured the test level
    Rebuilt test level to be more uniform. Updated a few properties.

---
+++ Prototype/Assets/Prefabs/Properties/LifespanProperty.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifespanProperty : MonoBehaviour
+{
+    [SerializeField]
+    private float lifeSpan;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        lifeSpan -= Time.deltaTime;
+        if (lifeSpan < 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
